c9c5cf2f42e0d4d553dd92583d7ff63a119fbe02
Rokas <rookas.rudzenskas@gmail.com>
2021-04-14 08:14:23 +0300

    Roaks

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,24 @@
 import React from "react";
+import {Container, Grid, Row, Col, Panel } from 'rsuite';
+import 'rsuite/dist/styles/rsuite-default.css';
 
 const SingIn = () => {
-    return <div>sign in</div>
+    return (
+        <Container>
+            <Grid>
+                <Row>
+                    <Col xs={24} md={12}>
+                        <Panel>
+                            <div>
+                                <h2>Welcome to the Secret Chat</h2>
+                                <p>Super Secret chat platform for developers</p>
+                            </div>
+                        </Panel>
+                    </Col>
+                </Row>
+            </Grid>
+    </Container>
+    );
 };
 
 export default SingIn;
