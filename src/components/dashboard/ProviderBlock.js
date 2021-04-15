44a2e3304019ed732a32b921bc60620c2e404b37
Rokas <rookas.rudzenskas@gmail.com>
2021-04-15 09:25:14 +0300

    Rokas

--- src/components/dashboard/ProviderBlock.js
+++ src/components/dashboard/ProviderBlock.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import { auth } from '../../misc/firebase';
-import {Button, Icon, Tag} from "rsuite";
+import {Alert, Button, Icon, Tag} from "rsuite";
+import firebase from "firebase/app";
 
 
 const ProviderBlock = () => {
@@ -10,27 +11,81 @@ const ProviderBlock = () => {
 
     });
 
+    const updateIsConnected = (providerId, value) => {
+        setIsConnected(p => {
+            return {
+                ...p,
+                [providerId]: value,
+            };
+        });
+    };
+
+
+    const unlink = async (providerId) => {
+        try {
+            if(auth.currentUser.providerData.length === 1) {
+                throw new Error(`You cannot disconnect from ${providerId}`);
+            }
+
+            await auth.currentUser.unlink(providerId);
+
+            updateIsConnected(providerId, false);
+            Alert.info(`Disconnected from ${providerId}`, 4000);
+        } catch (err) {
+            Alert.error(err.message, 4000);
+        }
+    }
+
+    const unlinkFacebook = () => {
+        unlink('facebook.com');
+    };
+    const unlinkGoogle = () => {
+        unlink('google.com');
+    };
+
+    const link = async (provider) => {
+        // (new firebase.auth.FacebookAuthProvider)
+        try {
+           await auth.currentUser.linkWithPopup(provider);
+           Alert.info(`Linked to ${provider.providerId}`, 4000);
+
+           updateIsConnected(provider.providerId, true);
+
+        } catch (err) {
+            Alert.error(err.message, 4000);
+
+        }
+    }
+
+
+    const linkFacebook = () => {
+      link(new firebase.auth.FacebookAuthProvider()) ;
+    };
+    const linkGoogle = () => {
+        link(new firebase.auth.GoogleAuthProvider());
+    };
+
     return(
         <div>
             {isConnected["google.com"] && (
-            <Tag closable color="green">
+            <Tag closable color="green" onClose={unlinkGoogle}>
                 <Icon icon="google" /> Connected
             </Tag>
             )}
             {isConnected["facebook.com"] && (
-            <Tag closable color="blue">
+            <Tag closable color="blue" onClose={unlinkFacebook}>
                 <Icon icon="facebook" /> Connected
             </Tag>
             )}
 
             <div className="mt-2">
                 {!isConnected["google.com"] &&
-                <Button block color="green">
+                <Button block color="green" onClick={linkGoogle}>
                     <Icon icon="google"/> Link to Google
                 </Button>
                 }
                 {!isConnected["facebook.com"] &&
-                <Button block color="blue">
+                <Button block color="blue" onClick={linkFacebook}>
                     <Icon icon="facebook"/> Link to Facebook
                 </Button>
                 }
