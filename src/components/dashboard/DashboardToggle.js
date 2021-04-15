6a596afeed66cfc4928902c891cb90f988005c19
Rokas <rookas.rudzenskas@gmail.com>
2021-04-15 07:55:51 +0300

    Rokas

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import {Button, Drawer, Icon} from "rsuite";
+import React, {useCallback} from 'react';
+import {Alert, Button, Drawer, Icon} from "rsuite";
 import {useMediaQuery, useModalState} from "../../misc/custom-hooks";
 import Dashboard from "./index";
+import {auth} from "../../misc/firebase";
 
 
-const DashboardToggle = () => {
+const DashboardToggle = ()  => {
 
     const {isOpen, close, open} = useModalState();
     const isMobile = useMediaQuery('(max-width: 992px)');
+
+    const onSignOut = useCallback(() => {
+        auth.signOut();
+
+        Alert.info("Signed Out", 4000);
+
+        close();
+
+    }, [close]);
 
     return (
         <>
@@ -15,7 +25,7 @@ const DashboardToggle = () => {
                 <Icon icon="dashboard" /> Dashboard
             </Button>
             <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
-                <Dashboard />
+                <Dashboard onSignOut={onSignOut}/>
             </Drawer>
         </>
     );
--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import {Drawer, Button} from "rsuite";
+import {useProfile} from "../../context/profile.context";
 
 
-const Dashboard = () => {
+const Dashboard = ({ onSignOut }) => {
+
+    const { profile } = useProfile();
+
     return (
-        <div>
-            hello
-        </div>
-    )
+        <>
+            <Drawer.Header>
+                <Drawer.Title>
+                    Dashboard
+                </Drawer.Title>
+            </Drawer.Header>
+            <Drawer.Body>
+
+                <h3>Hey, {profile.name}</h3>
+
+            </Drawer.Body>
+            <Drawer.Footer>
+                <Button block color="red" onClick={onSignOut}>
+                    Sign Out
+                </Button>
+            </Drawer.Footer>
+        </>
+    );
 };
 
 export default Dashboard;
