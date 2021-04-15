f30e472b5bfb7f0f1d21c0f42e67c728e2b3f30e
Rokas <rookas.rudzenskas@gmail.com>
2021-04-15 08:19:48 +0300

    Rokas

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {Drawer, Button} from "rsuite";
+import {Drawer, Button, Divider} from "rsuite";
 import {useProfile} from "../../context/profile.context";
+import EditableInput from "../EditableInput";
 
 
 const Dashboard = ({ onSignOut }) => {
 
     const { profile } = useProfile();
+    const onSave = async (newData) => {
+        console.log(newData);
+    };
 
     return (
         <>
@@ -17,6 +21,14 @@ const Dashboard = ({ onSignOut }) => {
             <Drawer.Body>
 
                 <h3>Hey, {profile.name}</h3>
+
+                <Divider />
+                <EditableInput
+                    name="nickname"
+                    initialValue={profile.name}
+                    onSave={onSave}
+                    label={<h6 className="mb-2">Nickname</h6>}
+                />
 
             </Drawer.Body>
             <Drawer.Footer>
