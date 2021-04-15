88598b5f36c7db8821e3263363bc217f2a191193
Rokas <rookas.rudzenskas@gmail.com>
2021-04-15 07:37:08 +0300

    Rokas

---
+++ src/components/dashboard/DashboardToggle.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Button, Drawer, Icon} from "rsuite";
+import {useModalState} from "../../misc/custom-hooks";
+import Dashboard from "./index";
+
+
+const DashboardToggle = () => {
+
+    const {isOpen, close, open} = useModalState()
+
+    return (
+        <>
+            <Button block color="blue" onClick={open}>
+                <Icon icon="dashboard" /> Dashboard
+            </Button>
+            <Drawer show={isOpen} onHide={close} placement="left">
+                <Dashboard />
+            </Drawer>
+        </>
+    );
+};
+
+export default DashboardToggle;
