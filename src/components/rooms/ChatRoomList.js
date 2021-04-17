229fbf36f96cda3d6a293057208a2bf0db7ac7d4
Rokas <rookas.rudzenskas@gmail.com>
2021-04-17 08:05:29 +0300

    Rokas

--- src/components/rooms/ChatRoomList.js
+++ src/components/rooms/ChatRoomList.js
@@ -2,15 +2,19 @@ import React from "react";
 import {Nav} from "rsuite";
 import RoomItem from "./RoomItem";
 
-const ChatRoomList = () => {
+const ChatRoomList = ({ aboveElHeight }) => {
     return (
-        <Nav appearance="subtle" vertical reversed className="overflow-y-scroll custom-scroll">
+        <Nav appearance="subtle" vertical reversed className="overflow-y-scroll custom-scroll"
+             style={{
+                 height: `calc(100% - ${aboveElHeight}px)`,
+             }}
+        >
             <Nav.Item>
                 <RoomItem />
             </Nav.Item>
 
         </Nav>
-    )
+    );
 };
 
 export default ChatRoomList
--- src/components/rooms/RoomItem.js
+++ src/components/rooms/RoomItem.js
@@ -1,11 +1,12 @@
 import React from "react";
+import TimeAgo from "timeago-react";
 
 const RoomItem = () => {
     return (
         <div>
             <div className="d-flex justify-content-between align-items-center">
                 <h3 className="text-disappear">Room name</h3>
-                <span>xx</span>
+                <TimeAgo datetime={new Date()} className="font-normal text-black-45"/>
             </div>
 
             <div className="d-flex align-items-center text-black-70">
