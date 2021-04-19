2b55b9582cecf4d712f37d90f260543979f93032
Rokas <rookas.rudzenskas@gmail.com>
2021-04-19 06:54:22 +0300

    Rokas

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,20 +2,37 @@ import React from "react";
 import {Col, Grid, Row} from "rsuite";
 import Sidebar from "../../components/Sidebar";
 import {RoomsProvider} from "../../context/rooms.context";
-import {Switch} from "react-router";
+import {Switch, useRouteMatch} from "react-router";
 import {Route} from "react-router-dom";
+import Chat from "./Chat";
+import {useMediaQuery} from "../../misc/custom-hooks";
 
 const Index = () => {
+
+    const isDesktop = useMediaQuery('(min-width: 992px)');
+    const { isExact } = useRouteMatch();
+    const canRenderSidebar = isDesktop || isExact;
+
     return(
     <RoomsProvider>
         <Grid fluid className="h-100">
             <Row className="h-100">
+                {canRenderSidebar &&
                 <Col xs={24} md={8} className="h-100">
                     <Sidebar/>
                 </Col>
+                }
+
                 <Switch>
                     <Route exact path="/chat/:chatId">
-
+                        <Col xs={24} md={16} className="h-100">
+                            <Chat/>
+                        </Col>
+                    </Route>
+                    <Route>
+                        {isDesktop && <Col xs={24} md={16} className="h-100">
+                            <h6 className="text-center mt-page">Please select active chat</h6>
+                        </Col>}
                     </Route>
                 </Switch>
             </Row>
