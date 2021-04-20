bfd75458b4c2530d887843a17d8e175278cca97c
Rokas <rookas.rudzenskas@gmail.com>
2021-04-20 07:48:49 +0300

    Rokas

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -2,6 +2,7 @@ import React from "react";
 import ProfileAvatar from "../../ProfileAvatar";
 import TimeAgo from "timeago-react";
 import ProfileInfoBtnModal from "./ProfileInfoBtnModal";
+import PresenceDot from "../../PresenceDot";
 
 const MessageItem = ({message}) => {
 
@@ -10,6 +11,7 @@ const MessageItem = ({message}) => {
     return (
         <li className="padded mb-1">
             <div className="d-flex align-items-center font-bolder mb-1">
+                <PresenceDot uid={author.uid}/>
                 <ProfileAvatar src={author.avatar} name={author.name} className="ml-1" size="xs" />
 
                 <ProfileInfoBtnModal profile={author} appearance="link" className="p-0 ml-1 text-black"/>
